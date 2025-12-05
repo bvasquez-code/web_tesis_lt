96d8a56aa875174a777fa5206a5c499f7ff9f17f
BVASQUEZ\bvasq <bvasquez>
2025-12-04 23:03:20 -0500

    Resumen de ejercicios resueltos

--- src/app/enterprise/student/model/dto/StudentWeakTopicsResponseDTO.ts
+++ src/app/enterprise/student/model/dto/StudentWeakTopicsResponseDTO.ts
@@ -1,8 +1,10 @@
 import { CourseWeaknessRankingDto } from "./CourseWeaknessRankingDto";
 import { ExamAttemptInfoDto } from "./ExamAttemptInfoDto";
+import { IExamResultDto } from "./IExamResultDto";
 import { WeakTopicDto } from "./WeakTopicDto";
 export class StudentWeakTopicsResponseDTO {
     WeakestTopics: WeakTopicDto[] = [];
     CourseWeaknessRanking: CourseWeaknessRankingDto[] = [];
     ExamAttemptInfo: ExamAttemptInfoDto[] = [];
+    ExamResults: IExamResultDto = new IExamResultDto();
 }
